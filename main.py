13ef742492c931444939f21c0be5e8c8c4db86fb
juandavid7777 <juandavid7777@gmail.com>
2022-09-05 16:01:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,7 @@ st.write('Values:', fstp)
 
 
 
-
-#----------------------------------------------
-
-#concrete properties
+# Material properties --------------------------
 Lf=0.1
 Ccon=800
 Kcon=1.7
@@ -32,7 +29,7 @@ Af=10
 Cw=Ccon*ro*Af*Lw
 Rwcon=Lw/(Kcon*Af)
 
-#----------------------------------------------
+# Model properties ----------------------------
 
 #air properties
 Cp_air=1000
@@ -41,25 +38,22 @@ V=30
 C_air=ro_air*Cp_air*V
 
 #slab film coefficient
-
 hf=5
 R12=1/(Af*hf)
 Cf=Ccon*ro*Af*Lf
 Cf1=Cf/2
 Cf2=Cf1
 
-#insulation for walls
+#Insulation for walls
 Rins=2; #U<0.57
 uw=1/Rins
 r0w=2*1/(uw*Af)
 
-#insulation for roof and floor
-
+#Insulation for roof and floor
 uff=1/4; #U<0.3
-r0f=1/uff*Af
+r0f=1/uff*Af;
 
-#thermal resistance of the slab
-
+#Thermal resistance of the slab
 Rf1f2=Lf/(2*Kcon*Af)
 R2f1=Lf/(4*Kcon*Af)
 Rf2g=R2f1
@@ -68,136 +62,243 @@ Rf2g=R2f1
 R1sw1=1/(7*10)
 
 #Window thermal resistance
-
 Awin=6
 uwin=1.5  #U<1.7
 rwin=1/(uwin*Awin)
 R1win=1/(5*Awin)
 
 #Thermal resistance between indoor air and outdoor air including infiltration, windows and doors
-
 R10=0.1
 Delta_t=50
 
-#------------------------------------------------------
-
-T1=np.zeros((1200,1))
-Tceiling=np.zeros((1200,1))
-T2=np.zeros((1200,1))
-Tf1=np.zeros((1200,1))
-Tf2=np.zeros((1200,1))
-Tsw1=np.zeros((1200,1))
-Tsw2=np.zeros((1200,1))
-Tbw=np.zeros((1200,1))
-Twin=np.zeros((1200,1))
-
-qaux=np.zeros((1200,1))
-Error=np.zeros((1200,1))
-PI=np.zeros((1200,1))
-
-T1[0]=25
-
-Tceiling[0]=25
-
-Tf1[0]=25
-
-Tf2[0]=25
-
-Tsw1[0]=25
-
-Tsw2[0]=25
-
-Tbw[0]=25
-
-Twin[0]=25
-
-
-qaux[0]=0
-
+#Maximum capacity of the HVAC system
 qmax=2000
 
+#Proportional controler constant
 Kp=1000
 
+#Ground temperature
 Tg=20
 
-import pandas as pd
+#Loads environmental data variables ---------------------------
 
-Data= pd.read_csv('Data.csv')
+#Import time series data variables (U)
+data= pd.read_csv('Data2.csv', parse_dates = ["date"], dayfirst  =True)
+data = data.set_index("date", drop = False)
 
-Too_july=Data["To"]
+#Estimates solar temperatures
+solar_vars = ['Qsolar_window', 'Qsolar_bw', 'Qsolar_roof', 'Qsolar_sw1', 'Qsolar_sw2',]
 
-QS_bw_july=Data["Qsolar_bw "]
-
-QS_sw1_july=Data["Qsolar_ sw1"]
-
-QS_sw2_july=Data["Qsolar_sw2"]
-
-QS_window_july=Data["Qsolar_window"]
-
-QS_ceil_july=Data["Qsolar_roof"]
-
-Tsol_bw=Too_july+(0.3*QS_bw_july/10);
-
-Tsol_sw1=Too_july+(0.3*QS_sw1_july/10);
-
-Tsol_sw2=Too_july+(0.3*QS_sw2_july/10);
-
-Tsol_ceil=Too_july+(0.3*QS_ceil_july/10);
-
-
-Tsp=np.ones((1200,1))*24
-
-Tsp2=np.ones((1200,1))*24
-
-Tsp2[791:830]=fstp
-
-Tsp2[1021:1060]=fstp
-
-#----------------------------------------------------
-
-pp=np.arange(0,1200-1)
-
-for i in pp:
+for var in solar_vars:
     
-    if Tsp2[i]<T1[i]:
+    suffix = var.split("_")
+    var_name = "Tsol_" + suffix[1]
+    data[var_name] = data["To"] + (0.3* data[var]/10)
+
+
+#slices selected dates data for a n day simulation --------------
+date_day_str = "2019-07-01"
+date_day = datetime.strptime(date_day_str,'%Y-%m-%d')
+
+d_5 = timedelta(days = 5)
+d_p1 = timedelta(days = 2)
+
+date_5dbefore = date_day-d_5
+date_1dafter = date_day+d_p1
+
+data_sim = data.loc[date_5dbefore:date_1dafter]
+
+#Set point definitions
+tsp =24
+data_sim["tsp"] = tsp
+data_sim["tsp2"] = tsp
+
+#Set point modification over period
+tsp2 = 20
+hour_s = 7    #hour start
+hour_e = 12   #hour end
+
+#Applies alternative setpoint
+data_sim.loc[date_day_str]["tsp2"] = data_sim.apply(lambda x: tsp2 if ((x["date"].hour >= hour_s) & (x["date"].hour < hour_e)) else tsp, axis = 1)
+data_sim["tsp2"].plot() #.loc[date_day_str]
+
+# Simulation
+#Set initialconditions
+state_vars = ["t1", "tceil", "t2", "tf1", "tf2", "tsw1", "tsw2", "tbw", "twin", "qaux", "error", "pi"]
+initial_values = [24,24,24,24,24,24,24,24,24,0,0,0]
+x0 = dict(zip(state_vars, initial_values))
+
+def simulate(data,x0,tsp = "tsp2"):
+
+    stats = defaultdict(list)
+
+    #Runs simulation
+    for i in range(1,len(data)+1):
+
+        #if else for the i-1 values depending on initial conditions
+        if i == 1:
+            t1_1 = x0["t1"]
+            tceil_1 = x0["tceil"]
+            t2_1 = x0["t2"]
+            tf1_1 = x0["tf1"]
+            tf2_1 = x0["tf2"]
+            tsw1_1 = x0["tsw1"]
+            tsw2_1 = x0["tsw2"]
+            tbw_1 = x0["tbw"]
+            twin_1 = x0["twin"]
+            qaux_1 = x0["qaux"]
+            error_1 = x0["error"]
+            pi_1 = x0["pi"]
+
+        else:
+            t1_1 = t1
+            tceil_1 = tceil
+            t2_1 = t2
+            tf1_1 = tf1
+            tf2_1 = tf2
+            tsw1_1 = tsw1
+            tsw2_1 = tsw2
+            tbw_1 = tbw
+            twin_1 = twin
+            qaux_1 = qaux
+            error_1 = error
+            pi_1 = pi
+
+        #Stores values for reporting
+        stats['t1'].append(t1_1)
+        stats['tceil'].append(tceil_1)
+        stats['t2'].append(t2_1)
+        stats['tf1'].append(tf1_1)
+        stats['tf2'].append(tf2_1)
+        stats['tsw1'].append(tsw1_1)
+        stats['tsw2'].append(tsw2_1)
+        stats['tbw'].append(tbw_1)
+        stats['twin'].append(twin_1)
+        stats['qaux'].append(qaux_1)
+        stats['error'].append(error_1)
+        stats['pi'].append(pi_1)
         
-        Error[i+1]=T1[i]-Tsp2[i]
-    else:
-        Error[i+1]=0
-    
-    PI[i+1]=Kp*Error[i]
-    if Error[i+1]>0.2:
-        qaux[i+1]=PI[i+1]
-    if PI[i+1]<qmax:
-        qaux[i+1]=PI[i+1]
-    if PI[i+1]>qmax:
-            qaux[i+1]=qmax
-   
-    Delta_t=50
-    
-    T1[i+1]=(Delta_t/(15*C_air))*(((T2[i]-T1[i])/R12)+((Too_july[i]-T1[i])/R10)+((Tsw1[i]-T1[i])/R1sw1)+((Tsw2[i]-T1[i])/R1sw1)+((Tbw[i]-T1[i])/R1sw1)+((Twin[i]-T1[i])/R1win)+((Tceiling[i]-T1[i])/R1sw1)-qaux[i])+T1[i]
-    
-    # T1[i+1]=(Delta_t/(15*C_air))*(((T2[i]-T1[i])/R12)+((Too_july[i]-T1[i]/R10))+((Tsw1[i]-T1[i])/R1sw1)+((Tsw2[i]-T1[i])/R1sw1)+((Tbw[i]-T1[i])/R1sw1)+((Twin[i]-T1[i])/R1win)+((Tceiling[i]-T1[i])/R1sw1)-qaux[i])+T1[i] 
-    
-    Tsw1[i+1]=(Delta_t/Cw)*(((T1[i]-Tsw1[i])/R1sw1)+((Tsol_sw1[i]-Tsw1[i])/(r0w+Rwcon)))+Tsw1[i]
-    
-    Tsw2[i+1]=(Delta_t/Cw)*(((T1[i]-Tsw2[i])/R1sw1)+((Tsol_sw2[i]-Tsw2[i])/(r0w+Rwcon)))+Tsw2[i]
-    
-    Tbw[i+1]=(Delta_t/Cw)*(((T1[i]-Tbw[i])/R1sw1)+((Tsol_bw[i]-Tbw[i])/(r0w+Rwcon))+(0.3*0.5*QS_window_july[i]*10))+Tbw[i]
-    
-    Tceiling[i+1]=(Delta_t/Cw)*(((T1[i]-Tceiling[i])/R1sw1)+((Tsol_ceil[i]-Tceiling[i])/(r0w+r0f)))+Tceiling[i]
+        #Controller modeling
+            #PI estimation
+        pi = Kp * error_1
 
-    Twin[i+1]=((T1[i]/R1sw1)+(Too_july[i]/rwin)+(0.1*QS_window_july[i]*6))/((1/R1sw1)+(1/rwin))
-                                   
-    T2[i+1]=((Tf1[i]/R2f1)+(T1[i]/R12)+(0.3*0.5*QS_window_july[i]*10))/((1/R2f1)+(1/R12))
-                                   
-    Tf1[i+1]=(Delta_t/Cf1)*(((Tf2[i]-Tf1[i])/Rf1f2)+((T2[i]-Tf1[i])/R2f1))+Tf1[i]
-                                   
-    Tf2[i+1]=(Delta_t/Cf2)*(((Tg-Tf2[i])/(Rf2g+r0f))+((Tf1[i]-Tf2[i])/Rf1f2))+Tf2[i]
+            #Error calculation
+        if data.iloc[i-1][tsp]<t1_1:
+            error = t1_1-data[tsp][i-1]
+
+        else:
+            error = 0
+
+            #Conditionals
+        if error>0.2:
+            qaux=pi
+
+        if (0 < pi) & (pi<qmax):
+            qaux=pi
+        else:
+            qaux = 0
+
+        if pi>qmax:
+            qaux=qmax
+
+        #Estimation of next time step
+        t1    = (Delta_t/(15*C_air))*(((t2_1-t1_1)/R12)+((data.iloc[i-1]["To"]-t1_1)/R10)+((tsw1_1-t1_1)/R1sw1)+((tsw2_1-t1_1)/R1sw1)+((tbw_1-t1_1)/R1sw1)+((twin_1-t1_1)/R1win)+((tceil_1-t1_1)/R1sw1)-qaux_1)+t1_1  
+        tsw1  = (Delta_t/Cw)*(((t1_1-tsw1_1)/R1sw1)+((data.iloc[i-1]["Tsol_sw1"]-tsw1_1)/(r0w+Rwcon)))+tsw1_1
+        tsw2  = (Delta_t/Cw)*(((t1_1-tsw2_1)/R1sw1)+((data.iloc[i-1]["Tsol_sw2"]-tsw2_1)/(r0w+Rwcon)))+tsw2_1
+        tbw   = (Delta_t/Cw)*(((t1_1-tbw_1)/R1sw1)+((data.iloc[i-1]["Tsol_bw"]-tbw_1)/(r0w+Rwcon))+(0.3*0.5*data.iloc[i-1]["Qsolar_window"]*10))+tbw_1
+        tceil = (Delta_t/Cw)*(((t1_1-tceil_1)/R1sw1)+((data.iloc[i-1]["Tsol_roof"]-tceil_1)/(r0w+r0f)))+tceil_1
+        twin  = ((t1_1/R1sw1)+(data.iloc[i-1]["To"]/rwin)+(0.1*data.iloc[i-1]["Qsolar_window"]*6))/((1/R1sw1)+(1/rwin))                              
+        t2    = ((tf1_1/R2f1)+(t1_1/R12)+(0.3*0.5*data.iloc[i-1]["Qsolar_window"]*10))/((1/R2f1)+(1/R12))                              
+        tf1   = (Delta_t/Cf1)*(((tf2_1-tf1_1)/Rf1f2)+((t2_1-tf1_1)/R2f1))+tf1_1                              
+        tf2   = (Delta_t/Cf2)*(((Tg-tf2_1)/(Rf2g+r0f))+((tf1_1-tf2_1)/Rf1f2))+tf2_1
+
+    #Results into dataframe
+    result = pd.DataFrame().from_dict(stats)
+    
+    return result
+
+#Simulate baseline data
+result = simulate(data_sim, x0, tsp = "tsp")
+data_sim = data_sim.reset_index(drop = True)
+df_bs = pd.concat([data_sim, result], axis=1).set_index("date", drop = False)
+
+
+#Simulate baseline data
+result = simulate(data_sim, x0, tsp = "tsp2")
+data_sim = data_sim.reset_index(drop = True)
+df_dr = pd.concat([data_sim, result], axis=1).set_index("date", drop = False)
 
     
-fig = plt.figure()
-plt.plot(qaux)
+fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-st.pyplot(fig)
+df_bs = df_bs.loc[date_day_str]
+df_dr = df_dr.loc[date_day_str]
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_bs['date'],
+    y=df_bs["qaux"],
+    mode = 'lines',
+    name = "Cooling baseline",
+    line = dict(width = 1.0, color = "red", dash = "solid")
+    ),secondary_y=False)
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_dr['date'],
+    y=df_dr["qaux"],
+    mode = 'lines',
+    name = "Cooling flexible",
+    line = dict(width = 2.0, color = "red", dash = "dash")
+    ),secondary_y=False)
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_bs['date'],
+    y=df_bs["tsp"],
+    mode = 'lines',
+    name = "Setpoint - baseline",
+    line = dict(width = 1.0, color = "blue", dash='solid')
+    ),secondary_y=True)
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_dr['date'],
+    y=df_dr["tsp2"],
+    mode = 'lines',
+    name = "Setpoint - flexible",
+    line = dict(width = 2.0, color = "blue", dash='dash')
+    ),secondary_y=True)
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_bs['date'],
+    y=df_bs["t1"],
+    mode = 'lines',
+    name = "Indoor air temperature",
+    line = dict(width = 1.0, color = "green", dash='solid')
+    ),secondary_y=True)
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_dr['date'],
+    y=df_dr["t1"],
+    mode = 'lines',
+    name = "Indoor air temperature",
+    line = dict(width = 2.0, color = "green", dash='dash')
+    ),secondary_y=True)
+
+
+#Adds metric
+fig.add_trace(go.Scatter(
+    x=df_dr['date'],
+    y=df_dr["To"],
+    mode = 'lines',
+    name = "Ambient Temperature",
+    line = dict(width = 1.0, color = "orange")
+    ),secondary_y=True)
+
+fig.update_yaxes(range=[0, 40], secondary_y = True)
+
+st.plotly_chart(fig)
 
