17a9b87c0eeeb1d9f9c5a6a55615c8f35f5e43fd
juandavid7777 <juandavid7777@gmail.com>
2022-09-06 10:24:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,8 @@ for var in solar_vars:
 
 
 #slices selected dates data for a n day simulation ---------------------------------------------
-date_day_str = "2019-07-01"
-date_day = date_day_select#datetime.strptime(date_day_str,'%Y-%m-%d')
+date_day_str = date_day_select.strftime()
+date_day = date_day_select #datetime.strptime(date_day_str,'%Y-%m-%d')
 
 d_5 = timedelta(days = 5)
 d_p1 = timedelta(days = 2)
