951e53ff7374ab98c631eaba8d65abc6b94553ca
juandavid7777 <juandavid7777@gmail.com>
2022-09-09 09:56:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ fig_dr_day.update_layout(
     )
 
 CADR_x = (datetime.combine(date_day_select,hour_e))
-fig_dr_day.add_annotation(x=CADR_x, y=df_dr["qaux"].max(),
+fig_dr_day.add_annotation(x=CADR_x, y=(df_dr["qaux"].max()-df_bs["qaux"].max())/2 + df_bs["qaux"].max(),
             text="CADR",
             showarrow=False)
 
