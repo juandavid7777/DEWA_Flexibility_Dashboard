1854dc2054e1d643c38c0c4ba0992ea273e4853d
juandavid7777 <juandavid7777@gmail.com>
2022-09-14 08:21:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -354,8 +354,8 @@ dr_cons = cooling_total_dr
 fig_gauge = go.Figure(go.Indicator(
     mode = "number+gauge+delta", value = dr_cons,
     domain = {'x': [0.1, 1], 'y': [0, 1]},
-    title = {'text' :"<b>Cooling load</b><br><span style='color: darkgray; font-size:0.8em'>kWh</span>"},
-    delta = {'reference': (dr_cons - bs_cons), "relative" : False},
+    title = {'text' :"<b>Cooling load</b><br><span style='color: darkgray; font-size:0.7em'>Baseline " + str(round(bs_cons,1)) + " kWh</span>"},
+    delta = {'reference': bs_cons, "relative" : True},
     gauge = {
         'shape': "bullet",
         'axis': {'range': [None, dr_cons]},
