69404a8eb6ad05f020da12e61a3a4c1b39073d1c
juandavid7777 <juandavid7777@gmail.com>
2022-09-15 13:32:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -362,17 +362,17 @@ dr_cons = cooling_total_dr
 fig_gauge = go.Figure(go.Indicator(
     mode = "number+gauge+delta", value = dr_cons,
     domain = {'x': [0.1, 1], 'y': [0, 1]},
-    title = {'text' :"<b>Cooling (kWh)</b><br><span style='color: dodgerblue; font-size:0.7em'>Baseline " + str(round(bs_cons,1)) + " kWh</span>"},
+    title = {'text' :"<b>Cooling (kWh)</b><br><span style='color: royalblue; font-size:0.7em'>Baseline " + str(round(bs_cons,1)) + " kWh</span>"},
     delta = {'reference': bs_cons, "relative" : True},
     gauge = {
         'shape': "bullet",
         'axis': {'range': [None, dr_cons*1.25]},
         'threshold': {
-            'line': {'color': "dodgerblue", 'width': 5},
+            'line': {'color': "royalblue", 'width': 5},
             'thickness': 0.75,
             'value': bs_cons},
         'steps': [
-            {'range': [0, bs_cons], 'color': "deepskyblue"},
+            {'range': [0, bs_cons], 'color': "dodgerblue"},
             {'range': [bs_cons, dr_cons], 'color': "lightsalmon"}],
         'bar':{'color':'darkblue'}
         }))
