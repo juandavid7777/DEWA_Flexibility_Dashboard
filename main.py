2d6c5c67a197cfb20826758dd3b15a14b2aab6b5
juandavid7777 <juandavid7777@gmail.com>
2022-09-15 12:08:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -368,7 +368,7 @@ fig_gauge = go.Figure(go.Indicator(
         'shape': "bullet",
         'axis': {'range': [None, dr_cons]},
         'threshold': {
-            'line': {'color': "k", 'width': 2},
+            'line': {'color': "gray", 'width': 2},
             'thickness': 0.75,
             'value': bs_cons},
         'steps': [
