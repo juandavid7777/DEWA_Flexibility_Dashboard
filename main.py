c2c59aad92217760a9081aaf85f78dcea1ec420c
juandavid7777 <juandavid7777@gmail.com>
2022-09-15 15:43:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -450,7 +450,7 @@ fig_CvsE.update_yaxes(range=[0, 50], title_text = "Ambient Temperature (C)", sec
 st.markdown('<b style="color:darkgoldenrod ; font-size: 44px">Flexibility analysis for a simplified building in Dubai</b>', unsafe_allow_html=True)
 
 st.markdown("""---""")
-st.markdown('<b style="color:midnightblue ; font-size: 30px">Ambient and model conditions</b>', unsafe_allow_html=True)
+st.markdown('<b style="color:midnightblue ; font-size: 20px">Ambient and model conditions</b>', unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
    st.image("Box_model.jpg")
@@ -459,11 +459,11 @@ with col2:
    st.plotly_chart(fig_dr_year, use_container_width=True)
 
 st.markdown("""---""")
-st.markdown('<b style="color:midnightblue ; font-size: 30px">HVAC efficiency</b>', unsafe_allow_html=True)
+st.markdown('<b style="color:midnightblue ; font-size: 20px">HVAC efficiency</b>', unsafe_allow_html=True)
 st.plotly_chart(fig_CvsE, use_container_width=True)
 
 st.markdown("""---""")
-st.markdown('<b style="color:midnightblue ; font-size: 30px">Model results</b>', unsafe_allow_html=True)
+st.markdown('<b style="color:midnightblue ; font-size: 20px">Model results</b>', unsafe_allow_html=True)
 col3, col4 = st.columns([1,2])
 with col3:
     st.plotly_chart(fig_gauge, use_container_width=True)
