e6038c8a944d180e012726606a104c9b21ed08fd
juandavid7777 <juandavid7777@gmail.com>
2022-09-16 11:44:26 +0400

    Update Optimize_setpoints.py

--- pages/Optimize_setpoints.py
+++ pages/Optimize_setpoints.py
@@ -134,7 +134,7 @@ cost_X.extend(list(np.ones(80) *35.6))
 with open('optimized_solutions.json', 'r') as fp:
     dict_sol = json.load(fp)
     
-cost, df = sim_elec_cost_full(dict_sol["24,20"], data_sim, date_day_str, cost_X)
+cost, df = sim_elec_cost_full(dict_sol[str(sp)+","+str(fstp)], data_sim, date_day_str, cost_X)
 
 #Estimates COP
 df["COP"] = df.apply(lambda x: chillerCOP(x["To"], x["qaux"]/2000*100), axis = 1)
