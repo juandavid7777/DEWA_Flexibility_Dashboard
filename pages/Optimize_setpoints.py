b793c17ce8d8925599135f09f420c44b7210b491
juandavid7777 <juandavid7777@gmail.com>
2022-09-16 11:16:28 +0400

    added json

--- pages/Optimize_setpoints.py
+++ pages/Optimize_setpoints.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
+import json
 
 from collections import defaultdict
 from matplotlib import pyplot as plt
 from scipy.integrate import cumtrapz as ctz
-from function import simulate, download_data_csv, roundTime, chillerCOP
+from function import simulate, download_data_csv, roundTime, chillerCOP, sim_elec_cost_full
 
 from datetime import datetime, timedelta, time
 
@@ -23,10 +24,9 @@ st.sidebar.image("gears.png")
 sps = st.sidebar.slider(
      'Comfort ranges',
      10, 35, (20,26))
-sp = sps[0]
-fstp = sps[1]
 
-st.write(sps[0], sps[1])
+sp = sps[1]
+fstp = sps[0]
 
 date_day_select = st.sidebar.date_input(
      "Analysis date",
@@ -35,4 +35,107 @@ date_day_select = st.sidebar.date_input(
      max_value = datetime(2019, 12, 30)
      )
 
-# cost, df = sim_elec_cost_full(X_tsp, data_sim, date_day_str, cost_X)
+setpoint_bool = st.sidebar.checkbox('Flexible event for prev. days')
+
+# Material properties -------------------------------------------------------------------------
+Lf=0.1
+Ccon=800
+Kcon=1.7
+ro=2200
+Ccon=800
+
+Lw=0.05
+Af=10
+Cw=Ccon*ro*Af*Lw
+Rwcon=Lw/(Kcon*Af)
+
+# Model properties -----------------------------------------------------------------------------
+#air properties
+Cp_air=1000
+ro_air=1.2
+V=30
+C_air=ro_air*Cp_air*V
+
+#slab film coefficient
+hf=5
+R12=1/(Af*hf)
+Cf=Ccon*ro*Af*Lf
+Cf1=Cf/2
+Cf2=Cf1
+
+#Insulation for walls
+Rins=2; #U<0.57
+uw=1/Rins
+r0w=2*1/(uw*Af)
+
+#Insulation for roof and floor
+uff=1/4; #U<0.3
+r0f=1/uff*Af
+
+#Thermal resistance of the slab
+Rf1f2=Lf/(2*Kcon*Af)
+R2f1=Lf/(4*Kcon*Af)
+Rf2g=R2f1
+
+#h thermal resistance between air and side/back wall
+R1sw1=1/(7*10)
+
+#Window thermal resistance
+Awin=6
+uwin=1.5  #U<1.7
+rwin=1/(uwin*Awin)
+R1win=1/(5*Awin)
+
+#Thermal resistance between indoor air and outdoor air including infiltration, windows and doors
+R10=0.1
+Delta_t=50
+
+#Maximum capacity of the HVAC system
+qmax=2000
+
+#Proportional controler constant
+Kp=1000
+
+#Ground temperature
+Tg=20
+
+#Loads environmental data variables -------------------------------------------------------------
+#Import time series data variables (U)
+data = download_data_csv('environment_data.csv') 
+data = data.set_index("date", drop = False)
+
+#slices selected dates data for a n day simulation ---------------------------------------------
+date_day_str = date_day_select.strftime("%Y-%m-%d")
+date_day = date_day_select #datetime.strptime(date_day_str,'%Y-%m-%d')
+
+d_5 = timedelta(days = 5)
+d_p1 = timedelta(days = 2)
+
+date_5dbefore = date_day-d_5
+date_1dafter = date_day+d_p1
+
+data_sim = data.loc[date_5dbefore:date_1dafter]
+
+#Set point definitions
+tsp = sp
+data_sim["tsp"] = tsp
+data_sim["tsp2"] = tsp
+
+#Set point modification over period
+tsp2 = fstp
+
+#Cost profile - define a price profile
+cost_X = list(np.ones(80) *0.8)
+cost_X.extend(list(np.ones(80) *1.4))
+cost_X.extend(list(np.ones(80) *0.8))
+
+#Gets optimal setpoint
+
+with open('optimized_solutions.json', 'r') as fp:
+    dict_sol = json.load(fp)
+    
+
+
+cost, df = sim_elec_cost_full(dict_sol["24,20"], data_sim, date_day_str, cost_X)
+
+st.write("Succes")
