423412f808a16fc65c2a8f0ca00906d6f7c09aae
juandavid7777 <juandavid7777@gmail.com>
2022-09-22 15:03:12 +0400

    Update Flexible_analysis.py

--- Flexible_analysis.py
+++ Flexible_analysis.py
@@ -519,7 +519,7 @@ dr_cons = cooling_total_dr
 fig_gauge_cool = go.Figure(go.Indicator(
     mode = "number+gauge+delta", value = dr_cons,
     domain = {'x': [0.1, 1], 'y': [0, 1]},
-    title = {'text' :"<b>Cooling. (kWh)</b><br><span style='color: royalblue; font-size:0.7em'>Baseline " + str(round(bs_cons,1)) + " kWh</span>"},
+    title = {'text' :"<b>Cooling. (kWh)</b><br><span style='color: royalblue; font-size:0.7em'>Baseline " + str(round(bs_cons,2)) + " kWh</span>"},
     delta = {'reference': bs_cons, "relative" : True, 'valueformat':".2%"},
     gauge = {
         'shape': "angular",
@@ -542,7 +542,7 @@ dr_cons = elec_total_dr
 fig_gauge_elec = go.Figure(go.Indicator(
     mode = "number+gauge+delta", value = dr_cons,
     domain = {'x': [0.1, 1], 'y': [0, 1]},
-    title = {'text' :"<b>Elect. (kWh)</b><br><span style='color: royalblue; font-size:0.7em'>Baseline " + str(round(bs_cons,1)) + " kWh</span>"},
+    title = {'text' :"<b>Elect. (kWh)</b><br><span style='color: royalblue; font-size:0.7em'>Baseline " + str(round(bs_cons,2)) + " kWh</span>"},
     delta = {'reference': bs_cons, "relative" : True, 'valueformat':".2%"},
     gauge = {
         'shape': "angular",
