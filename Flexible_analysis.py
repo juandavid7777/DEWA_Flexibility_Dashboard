aeda3295e653e3899608cee097f151d0693772d9
juandavid7777 <juandavid7777@gmail.com>
2022-09-26 11:45:40 +0400

    Update Flexible_analysis.py

--- Flexible_analysis.py
+++ Flexible_analysis.py
@@ -264,6 +264,8 @@ df_elec_table = pd.DataFrame({"CADR (kWh)":[elec_down_flex],
 "Peak-h reduction (kW)": [elec_befi],
 "Peak-h reduction (%)": [elec_befi_p]})
 
+df_elec_table["CADR (kWh)"] = df_elec_table["CADR (kWh)"].map('${:,.2f}'.format)
+
 # df_elec_table.style.format({"CADR (kWh)": "{:.2%}",
 # "Energy shift (kWh)" : "{:.2%}",
 # "CADR/E.Shift (%)": "{:.2%}",
