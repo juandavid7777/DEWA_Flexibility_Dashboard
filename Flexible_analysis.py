c84c6ddc8b357e6ebae5ad1a3d32870f9184e09c
juandavid7777 <juandavid7777@gmail.com>
2022-09-26 11:35:11 +0400

    Update Flexible_analysis.py

--- Flexible_analysis.py
+++ Flexible_analysis.py
@@ -264,7 +264,12 @@ df_elec_table = pd.DataFrame({"CADR (kWh)":[elec_down_flex],
 "Peak-h reduction (kW)": [elec_befi],
 "Peak-h reduction (%)": [elec_befi_p]})
 
-df_elec_table.style = df_elec_table.style.format({'Quantity': "{:.2%}"})
+df_elec_table.style = df_elec_table.style.format({"CADR (kWh)": "{:.2%}",
+"Energy shift (kWh)" : "{:.2%}",
+"CADR/E.Shift (%)": "{:.2%}",
+"Peak-h reduction (kW)": "{:.2%}",
+"Peak-h reduction (%)": "{:.2%}",
+})
 
 #Plotting -------------------------------------------------------------------------------------   
 fig_dr_day = make_subplots(specs=[[{"secondary_y": True}]])
